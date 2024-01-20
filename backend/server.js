99bfbb9f269431ab2d4679b30273cc0ed3ec6ae4
RCRvtYnQ1 <ameya.rathod@research.iiit.ac.in>
2024-01-20 19:54:27 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const PORT = 4000;
-const DB_NAME = "tutorial"
+const PORT = process.env.PORT || 4000;
 
 // routes
 var testAPIRouter = require("./routes/testAPI");
@@ -13,13 +11,6 @@ var UserRouter = require("./routes/Users");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/' + DB_NAME, { useNewUrlParser: true });
-const connection = mongoose.connection;
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully !");
-})
 
 // setup API endpoints
 app.use("/testAPI", testAPIRouter);
